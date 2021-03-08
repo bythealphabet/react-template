dcc38069ef672d59002714cc2d096db43d43e3d0
bythealphabet <bythealphabet@gmail.com>
2021-03-08 01:47:13 -0400

    adding open browser to project

--- build-utils/webpack.development.js
+++ build-utils/webpack.development.js
@@ -1,9 +1,10 @@
 module.exports = () => {
-  return {
-    devtool: "eval-source-map",
-    devServer: {
-      port: 3000,
-      hot: true,
-    },
-  };
+	return {
+		devtool: "eval-source-map",
+		devServer: {
+			port: 3000,
+			hot: true,
+			open: true,
+		},
+	};
 };
